8e87cf040e213479b331e87b2e513454c791633f
Vinit <laddhavinit99@gmail.com>
2026-05-31 22:23:24 +0530

    Reactive forms

--- ReactiveForms/src/app/app.ts
+++ ReactiveForms/src/app/app.ts
@@ -1,12 +1,21 @@
 import { Component, signal } from '@angular/core';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ReactiveFormsModule ],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('ReactiveForms');
+  email=new FormControl("")
+  pass=new FormControl("")
+
+  login(){
+    console.log(this.email.value);
+    console.log(this.pass.value);
+    
+  }
 }
