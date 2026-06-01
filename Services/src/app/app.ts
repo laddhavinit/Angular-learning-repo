1231ec56fe7b1111c3303201add3383a2f8db635
Vinit <laddhavinit99@gmail.com>
2026-06-01 10:42:07 +0530

    Services

--- Services/src/app/app.ts
+++ Services/src/app/app.ts
@@ -1,5 +1,6 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ProductService } from './services/product-service';
 
 @Component({
   selector: 'app-root',
@@ -8,5 +9,13 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.scss'
 })
 export class App {
+
+  productData:any=signal("")
   protected readonly title = signal('Services');
+  constructor(private productservice: ProductService){}
+  ngOnInit(){
+    const data=this.productservice.getProducts()
+    console.log(data);
+    this.productData.set(data)
+  }
 }
--- Services/src/app/services/product-service.ts
+++ Services/src/app/services/product-service.ts
@@ -3,4 +3,15 @@ import { Injectable } from '@angular/core';
 @Injectable({
   providedIn: 'root',
 })
-export class ProductService {}
+export class ProductService {
+  constructor(){
+    console.log("Service Called");
+  }
+  getProducts(){
+    return [
+      {id:1, name:'Adi'},
+      {id:2, name:'Raj'},
+      {id:3, name:'Vinit'}
+    ]
+  }
+}
